2dc83659875e84f023ce62b3e10fadac56db249c
YinRJ <yin_rj@qq.com>
2022-03-27 12:52:10 +0800

    项目初始化

---
+++ src/main/java/com/yinrj/emoswxapi/EmosWxApiApplication.java
@@ -0,0 +1,13 @@
+package com.yinrj.emoswxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmosWxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmosWxApiApplication.class, args);
+    }
+
+}
